40f6fff82649829012a9605c0b7e8646825a4441
Sahar Ali <sahar.aliabdou@gmail.com>
2022-03-10 14:12:04 +0100

    Add project files.

---
+++ EmployeeFetchApi/Model/Employee.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmployeeFetchApi
+{
+    public class Employee
+    {
+        public string status { get; set; }
+        public EmployeeData[] data { get; set; }
+        public string message { get; set; }
+    }
+}
---
+++ EmployeeFetchApi/Program.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Net.Http.Json;
+
+//using System.Text.Json.Serialization;
+using System.Text.Json;
+using System.Collections.Generic;
+using Newtonsoft.Json;
+
+namespace EmployeeFetchApi
+{
+    class Program
+    {/// <summary>
+     /// This application will do a call to this api "http://dummy.restapiexample.com/api/v1/employees"
+     /// And then lists the employees in a table.
+
+     /// </summary>
+     /// <param name="args"></param>
+     /// <returns></returns>
+      static async Task Main(string[] args)
+        {
+            Console.ForegroundColor = ConsoleColor.DarkYellow;
+
+            HttpClient client = new HttpClient();
+            try
+            {
+                var table = new Table();
+                table.SetHeaders("ID", "Name", "Salary", "Age", "Profile Image");
+                var response = await client.GetStringAsync("http://dummy.restapiexample.com/api/v1/employees");
+
+
+                Employee _employeestatus = JsonConvert.DeserializeObject<Employee>(response); //Converts to c# object
+
+
+                // iterate over the values in data array:
+                // we have to iterate over data because we have multiple variables
+                Console.WriteLine(_employeestatus.status);
+
+                foreach (var _employee in _employeestatus.data)
+                {
+
+
+                    table.AddRow(_employee.id.ToString(), _employee.employee_name, _employee.employee_age.ToString(), _employee.employee_salary.ToString(), _employee.profile_image);
+
+
+                }
+
+                Console.WriteLine(table.ToString());
+
+                Console.WriteLine(_employeestatus.message);
+            }
+            catch (HttpRequestException e)
+            {
+                Console.ForegroundColor = ConsoleColor.Red;
+
+                Console.WriteLine(e.Message + "Please try again later!");
+
+            }
+
+
+
+
+        }
+
+
+
+
+
+    }
+
+}
